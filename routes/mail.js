1f0cf700ba5046b677d41e42e17c128e96ce799f
Funwi Che <78383345+chefidelis@users.noreply.github.com>
2023-08-06 14:55:39 +0100

    2023.08.06.14.00

--- routes/mail.js
+++ routes/mail.js
@@ -5,23 +5,22 @@ const nodemailer = require("nodemailer");
 
 router.post("", async (req, res) => {
   try {
-    let info;
     const { host, user, subject, replyTo, html, from, to, attachments, copy } =
       req.body;
     const auth = { user, pass: process.env.PASS };
     if (attachments && attachments.length)
-      info = await nodemailer
+      await nodemailer
         .createTransport({ host, port: 465, secure: true, auth })
         .sendMail({ from, to, replyTo, subject, html, attachments });
     else
-      info = await nodemailer
+      await nodemailer
         .createTransport({ host, port: 465, secure: true, auth })
         .sendMail({ from, to, replyTo, subject, html });
-    if (info && copy)
+    if (copy)
       await nodemailer
         .createTransport({ host, port: 465, secure: true, auth })
         .sendMail(copy);
-    console.log("message sent %s", info.messageId);
+    console.log("message sent");
     res.sendStatus(200);
   } catch (error) {
     console.log(error);
