5306f876fe72d9d72c9b461aabaeb44ff6b9af3e
Funwi Che <chefidelis@gmail.com>
2024-03-10 08:16:45 +0100

    24.3.10.8.16

--- app.js
+++ app.js
@@ -14,16 +14,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "*");
   next();
 });
-app.use(express.static(path.join(__dirname, "public")));
-// Global Middlewares
 app.use(express.json({ limit: "50mb" }));
-app.use(cors());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  next();
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/mail", require("./routes/mails"));
