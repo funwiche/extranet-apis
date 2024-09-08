5aeba6af02f9307cfcb6fb1db6e8416920bc4cb9
Funwi Che <funwiche.dev@gmail.com>
2024-09-08 15:23:25 +0100

    Update privateemail.js

--- routes/privateemail.js
+++ routes/privateemail.js
@@ -25,5 +25,26 @@ router.post("", async (req, res) => {
     res.sendStatus(500);
   }
 });
+router.post("/hogland", async (req, res) => {
+  try {
+    let { to, subject, name, email, phone, location, message } = req.body;
+    const from = `"${name}" <${to}>`;
+    const auth = { user: to, pass: process.env.PASS_HOGLAND };
+    const host = "mail.privateemail.com";
+    html = ` 
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Full name:</strong> ${name}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Email Address: </strong>${email}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Phone Number:</strong> ${phone}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Country / Region:</strong> ${location}</div>
+    <div style="font-size:14px;margin-bottom:8px;"><strong>Message: </strong>${message}`;
+    await nodemailer
+      .createTransport({ port: 465, secure: true, host, auth })
+      .sendMail({ from, to, replyTo: email, subject, html });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
 
 module.exports = router;
